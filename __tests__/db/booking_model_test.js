dd87d1d5fe9db46d618a4728150251550dcb5b4d
up1 <somkiat.p@gmail.com>
2020-10-18 15:59:30 +0700

    Working with data-driven testing

---
+++ __tests__/db/booking_model_test.js
@@ -0,0 +1,35 @@
+const BookingModel = require("../../src/db/booking_model");
+
+test("should valid model", async () => {
+  const model = new BookingModel({
+    party: 1,
+    name: "pui",
+    email: "email@email.com",
+    date: "2020/10/18",
+    time: "04:00 PM",
+    phone: "",
+    message: "",
+  });
+  await expect(model.validator()).resolves.toStrictEqual(model);
+});
+
+test.each`
+  party | name         | email
+  ${1}  | ${"user 01"} | ${"email@1.com"}
+  ${2}  | ${"user 02"} | ${"email@2.com"}
+  ${3}  | ${"user 03"} | ${"email@3.com"}
+`(
+  "Data-driven testing with $party and name=$name",
+  async ({ party, name, email }) => {
+    const model = new BookingModel({
+      party: party,
+      name: name,
+      email: email,
+      date: "2020/10/18",
+      time: "04:00 PM",
+      phone: "",
+      message: "",
+    });
+    await expect(model.validator()).resolves.toStrictEqual(model);
+  }
+);
